a1b362c81faee34b0dca97489ead525dad721634
George Spătăcean <george.spatacean@gmail.com>
2024-05-18 12:55:47 +0300

    Add basic powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+oh-my-posh init pwsh --config "$env:USERPROFILE\.config\customized_atomic.omp.json" | Invoke-Expression
+
+New-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
