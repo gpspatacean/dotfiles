889b9d242dd0acdb16c29ad61856b9b49c6ecfea
George Spătăcean <george.spatacean@gmail.com>
2024-05-19 12:05:39 +0300

    Add base nvim config
    Taken step by step from modular nvim kickstart repo

---
+++ .config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,11 @@
+require('lazy').setup({
+    'tpope/vim-sleuth',
+    require 'plugins/telescope',
+    require 'plugins/tokyonight',
+    require 'plugins/which-key',
+    require 'plugins/treesitter',
+    require 'plugins/lualine',
+    require 'plugins/cmp',
+    require 'plugins/lspconfig'
+
+});
