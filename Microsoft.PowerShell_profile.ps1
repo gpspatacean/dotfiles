d3b5f91ea2baef6a31874282b8d1b3b8a7be7473
George Spătăcean <george.spatacean@gmail.com>
2024-05-19 12:38:55 +0300

    Add PS git aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,14 @@
 oh-my-posh init pwsh --config "$env:USERPROFILE\.config\customized_atomic.omp.json" | Invoke-Expression
 
 New-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
+
+function git-status {
+	git status
+}
+
+function git-log {
+	git lg
+}
+
+New-Alias -Name gst -Value git-status
+New-Alias -Name glg -Value git-log
