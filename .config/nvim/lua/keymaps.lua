3e5e70e7d9f09b8f086769e984d12abc15f5d516
George Spătăcean <george.spatacean@gmail.com>
2024-06-16 13:07:41 +0300

    Map nvim window splits keys same as tmux pane splits

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,8 +2,8 @@ vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with \"jk\"" });
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>",  { desc = "Clear search highlights" });
 
 -- Splits management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
+vim.keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Split window vertically"})
+vim.keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split window horizontally"})
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits size equal"})
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
 
