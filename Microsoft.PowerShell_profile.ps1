06a3d1ebfaff50c8e73771bf12d012ff152e168b
George Spătăcean <george.spatacean@gmail.com>
2025-02-17 21:49:42 +0200

    pwsh: bat/eza
    Initial setup for bat/eza and fzf preview integration

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
 #Init oh-my-posh; theme should be present
 oh-my-posh init pwsh --config "$env:USERPROFILE\.config\customized_atomic.omp.json" | Invoke-Expression
 
+#Fzf (Import the fuzzy finder and set a shortcut key to begin searching)
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
 #Notepad++
 Set-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
@@ -20,6 +24,12 @@ function nppd_method {
 }
 Set-Alias -Name nppd -Value nppd_method
 
+# Utility Command that tells you where the absolute path of commandlets are
+function which ($command) {
+ Get-Command -Name $command -ErrorAction SilentlyContinue |
+ Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
 #Git related stuff
 function git-status {
 	git status
@@ -29,7 +39,35 @@ function git-log {
 	git lg
 }
 
+# Aliases
 Set-Alias -Name gst -Value git-status
 Set-Alias -Name glg -Value git-log
 
 Set-Alias -Name lg -Value lazygit
+
+function eza-cmd {
+	param(
+		[Parameter(ValueFromRemainingArguments = $true)]
+		[String[]]$PassThruParams
+	)
+
+	$command = "eza -alT --classify=always --color=always --icons=always --show-symlinks -L=2 "
+	if($PassThruParams) {
+		$command += " " + ($PassThruParams -join " ")
+	}
+	Invoke-Expression $command
+}
+#Remove-Alias -Name ls
+Set-Alias -Name ls -Value eza-cmd -Option AllScope
+
+
+# Utility Command that tells you where the absolute path of commandlets are
+function which ($command) {
+ Get-Command -Name $command -ErrorAction SilentlyContinue |
+ Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Environment Variables
+$env:BAT_CONFIG_PATH="$env:USERPROFILE\.config\bat\config" #`bat` config file path
+$env:FZF_CTRL_T_OPTS = "--preview 'if exist {}/ ( eza -alT --classify=always --color=always --icons=always --show-symlinks --git-ignore -L=1 {} ) else ( bat {} )'"
+$env:FZF_ALT_C_OPTS="--preview 'eza -alT --classify=always --color=always --icons=always --show-symlinks --git-ignore -L=2 {}'"
--- README.md
+++ README.md
@@ -7,3 +7,23 @@ Run ```:PluginInstall``` in vim
 ### Tmux
 Install plugin manager: ```git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm```
 Press ```Prefix + I``` to install
+
+### fzf
+
+### eza
+https://github.com/eza-community/eza
+
+```bash
+wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
+sudo chmod +x eza
+sudo chown root:root eza
+sudo mv eza /usr/local/bin/eza
+```
+`winget install eza-community.eza`
+
+### bat
+https://github.com/sharkdp/bat
+
+`sudo apt install bat`
+
+`winget install sharkdp.bat `
