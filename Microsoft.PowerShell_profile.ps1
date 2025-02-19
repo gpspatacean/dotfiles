8fde058ce1a00ddb099f9adde2559361b65a6cc3
George Spătăcean <george.spatacean@gmail.com>
2025-02-19 23:59:28 +0200

    pwsh: Declare aliases directly at function level

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,36 +10,38 @@ Set-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
 #Open a file in a new notepad instance
 function nppi_method {
+	[alias('nppi')]
 	param([String]$Target)
 	#new notepad++ instance; do not open files from other sessions
 	npp -multiInst -nosession $Target
 }
-Set-Alias -Name nppi -Value nppi_method
 
 #Open a directory as a workspace
 function nppd_method {
+	[alias('nppd')]
 	param([String]$Target)
 	#open directory as a workspace; new instance; do not open files from other session
 	npp -openFoldersAsWorkspace -multiInst -nosession $Target
 }
-Set-Alias -Name nppd -Value nppd_method
 
 #Git related stuff
 function git-status {
+	[alias('gst')]
+	param()
 	git status
 }
 
 function git-log {
+	[alias('glg')]
+	param()
 	git lg
 }
 
 # Aliases
-Set-Alias -Name gst -Value git-status
-Set-Alias -Name glg -Value git-log
-
 Set-Alias -Name lg -Value lazygit
 
 function eza-cmd {
+	[alias('ls')]
 	param(
 		[Parameter(ValueFromRemainingArguments = $true)]
 		[String[]]$PassThruParams
@@ -51,9 +53,6 @@ function eza-cmd {
 	}
 	Invoke-Expression $command
 }
-#Remove-Alias -Name ls
-Set-Alias -Name ls -Value eza-cmd -Option AllScope
-
 
 # Utility Command that tells you where the absolute path of commandlets are
 function which ($command) {
