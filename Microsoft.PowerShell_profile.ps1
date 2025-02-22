42600cdea57f5b742903cc2142eaee84b169ef87
George Spătăcean <george.spatacean@gmail.com>
2025-02-22 12:48:50 +0200

    pwsh: set XDG_CONFIG_HOME and use it

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -61,6 +61,7 @@ function which ($command) {
 }
 
 # Environment Variables
-$env:BAT_CONFIG_PATH="$env:USERPROFILE\.config\bat\config" #`bat` config file path
+$env:XDG_CONFIG_HOME="$env:USERPROFILE\.config"
+$env:BAT_CONFIG_PATH="$env:XDG_CONFIG_HOME\bat\config" #`bat` config file path
 $env:FZF_CTRL_T_OPTS = "--preview 'if exist {}/ ( eza -al --classify=always --color=always --icons=always --show-symlinks --git-ignore --follow-symlinks {} ) else ( bat {} )'"
 $env:FZF_ALT_C_OPTS="--preview 'eza -alT --classify=always --color=always --icons=always --show-symlinks --git-ignore --follow-symlinks -L=2 {}'"
