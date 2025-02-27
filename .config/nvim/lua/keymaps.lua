e89bb107feaace54473c7707dd8ef937303426f0
George Spătăcean <george.spatacean@gmail.com>
2025-02-28 00:09:10 +0200

    nvim: Add some quick lua exec shortcuts

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split window horizontally"
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits size equal"})
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
 
+-- Buffers
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<CR>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<Tab><Tab>", "<cmd>ls<CR>", { desc = "List Buffers" })
@@ -17,4 +18,9 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current t
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Move to next tab"}) -- 'gt'
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Move to previous tab"}) -- 'gT'
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current file in new tab"})
+
+-- Lua shortcuts
+vim.keymap.set("n", "<leader>ls", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>lx", ":.lua<CR>")
+vim.keymap.set("v", "<leader>lx", ":lua<CR>")
 
